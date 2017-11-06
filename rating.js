a44b9ec2cf7ce0b31d3c5479053ed54f117ac250
Jean Liu <jean4599@gmail.com>
2017-11-06 18:33:34 +0800

    self define rating system, and change the layout in screenshot and text

---
+++ rating.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$('.rating').each(function(){
+		var value = $(this).attr('data-rating');
+		for(var i=0; i<value; i++){
+			$(this).append("<img class='icon-chef' src='./img/chef-yellow.png'/>");
+		}
+		for(var i=value; i<5; i++){
+			$(this).append("<img class='icon-chef' src='./img/chef-grey.png'/>");
+		}
+	})
+
+})
