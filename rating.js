3e41474e814af41c86f2ef36b2a7091ae98ab447
thatcharles <charles5j21z@gmail.com>
2017-11-07 13:20:09 +0800

    add content

---
+++ rating.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$('.rating').each(function(){
+		var value = $(this).attr('data-rating');
+		for(var i=0; i<value; i++){
+			$(this).append("<img class='icon-chef' src='./img/chef-yellow.png'/>");
+		}
+		for(var i=value; i<5; i++){
+			$(this).append("<img class='icon-chef' src='./img/chef-grey.png'/>");
+		}
+		//tooltip
+		$(this).append("<div class='tooltip'>tooltips<br>tooltips2<br>tooltips3</div>");
+	})
+})
